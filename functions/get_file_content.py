049015955a7d0a76a969fc54c18ce9b7dceb5044
Julian Aladio <jmaladio@gmail.com>
2025-11-02 17:14:27 +0000

    feat: add get_file_content fn and create tests

---
+++ functions/get_file_content.py
@@ -0,0 +1,27 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_target_path = os.path.abspath(os.path.join(abs_working_directory, file_path))
+
+    if not (abs_target_path == abs_working_directory or abs_target_path.startswith(abs_working_directory + os.sep)):
+        return f'Error: Cannot read "{abs_target_path}" as it is outside the permitted working directory'
+
+    if not (os.path.isfile(abs_target_path)):
+        return f'Error: File not found or is not a regular file: "{abs_target_path}"'
+
+
+    try:
+        with open(abs_target_path, "r") as f:
+            file_size_bytes = os.path.getsize(abs_target_path)
+            file_content_string = ''
+            if (file_size_bytes > MAX_CHARS):
+                file_content_string = f.read(MAX_CHARS) + f'[...File "{abs_target_path}" truncated at {MAX_CHARS} characters]'
+            else:
+                file_content_string = f.read()
+
+            return file_content_string
+
+    except Exception as e:
+        return f'Error: {e}'
--- tests.py
+++ tests.py
@@ -1,14 +1,16 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-result_a = get_files_info("calculator", ".")
+result_a = get_file_content("calculator", "lorem.txt")
 print(result_a)
 
-result_b = get_files_info("calculator", "pkg")
+result_b = get_file_content("calculator", "main.py")
 print(result_b)
 
-result_c = get_files_info("calculator", "/bin")
+result_c = get_file_content("calculator", "pkg/calculator.py")
 print(result_c)
 
-result_d = get_files_info("calculator", "../")
+result_d = get_file_content("calculator", "/bin/cat")
 print(result_d)
 
+result_e = get_file_content("calculator", "pkg/does_not_exist.py")
+print(result_e)
