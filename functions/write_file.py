1b9fd885634da47b6a50357bef90b1b39a289d95
Julian Aladio <jmaladio@gmail.com>
2025-11-03 09:39:25 +0000

    feat: add write_file fn

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,15 +5,17 @@ def write_file(working_directory, file_path, content):
         abs_working_directory = os.path.abspath(working_directory)
         abs_target_path = os.path.abspath(os.path.join(abs_working_directory, file_path))
 
-        if not (abs_target_path == abs_working_directory or abs_target_path.startswith(abs_working_directory + os.sep)):
-            return f'Error: Cannot write to "{abs_target_path}" as it is outside the permitted working directory'
+        if not (abs_target_path.startswith(abs_working_directory + os.sep)):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
 
-        if not (os.path.exists(abs_target_path)):
-            os.makedirs(abs_target_path)
-            with open(abs_target_path, "w") as f:
-                f.write(content)
+        parent_dir = os.path.dirname(abs_target_path)
+        if not (os.path.exists(parent_dir)):
+            os.makedirs(parent_dir, exist_ok=True)
 
-            return f'Successfully wrote to "{file_path} ({len(content)} characters written)"'
+        with open(abs_target_path, "w") as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
 
     except Exception as e:
-        raise f'Error: {e}'
+        return f'Error: {e}'
