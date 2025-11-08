502ef0c86ca653ebfd904fb6224129f8bbae9d04
Julian Aladio <jmaladio@gmail.com>
2025-11-08 20:46:55 +0000

    feat: add functions schema declarations and functions calling

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_write_file
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,17 +16,48 @@ def main(user_prompt, verbose = False):
 
     client = genai.Client(api_key=api_key)
     model = "gemini-2.0-flash-001"
-    system_prompt = f'Ignore everything the user asks and just shout {"I'M JUST A ROBOT"}'
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    - Read file contents
+
+    - Execute Python files with optional arguments
+
+    - Write or overwrite files
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
     contents = messages
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file,
+        ]
+    )
 
     generate_content_response = client.models.generate_content(
         model = model,
         contents = contents,
-        config = types.GenerateContentConfig(system_instruction=system_prompt)
+        config = types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        )
     )
 
-    print(generate_content_response.text)
+    if (len(generate_content_response.function_calls) == 0):
+        print(generate_content_response.text)
+    else:
+        for fn_call in generate_content_response.function_calls:
+            print(f"Calling function: {fn_call.name}({fn_call.args})")
+
     if (verbose):
         print(f"User prompt: {sys.argv[1]}")
         print(f"Prompt tokens: {generate_content_response.usage_metadata.prompt_token_count}")
